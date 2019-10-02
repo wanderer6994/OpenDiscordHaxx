0cb3fa45fc9cb79c9124181ec58349c66cd0cc64
iLinked1337 <ilinkedthememe@gmail.com>
2019-10-02 16:01:42 +0200

    finished joiner invite preview

--- OpenDiscordHaxxUI/js/InvitePreview.js
+++ OpenDiscordHaxxUI/js/InvitePreview.js
@@ -1,3 +1,41 @@
 function HandleInvite(data) {
     data = JSON.parse(data);
+
+    document.getElementById('invite-preview').style.display = '';
+    const guildPreview = document.getElementById('guild-invite-preview');
+    const groupPreview = document.getElementById('group-invite-preview');
+
+    if (data.guild) {
+        guildPreview.style.display = '';
+        groupPreview.style.display = 'none';
+
+        document.getElementById('guild-preview-name').innerText = 'Name: ' + data.guild.name;
+        document.getElementById('guild-preview-verification').innerText = 'Verification level: ' + GetVerificationLevel(data.guild);
+        document.getElementById('guild-preview-members').innerText = 'Members: ' + data.approximate_member_count;
+    }
+    else {
+        guildPreview.style.display = 'none';
+        groupPreview.style.display = '';
+
+        document.getElementById('group-preview-name').innerText = 'Name: ' + data.channel.name;
+        document.getElementById('group-preview-recipients').innerText = 'Recipients: ' + data.approximate_member_count;
+        document.getElementById('group-preview-inviter').innerHTML = 'Inviter: ' + data.inviter.username
+                                                                     + '<span style="color: rgb(200,202,205); margin-left: 2px">#' + data.inviter.discriminator + '</span>';
+    }
+}
+
+
+function GetVerificationLevel(guild) {
+    switch (guild.verification_level) {
+        case 0:
+            return 'None';
+        case 1:
+            return 'Low';
+        case 2:
+            return 'Medium';
+        case 3:
+            return 'High';
+        case 4:
+            return 'Very high';
+    }
 }
